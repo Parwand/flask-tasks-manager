3292dc02fc7204a077330df8068a0fa30726a3a2
Parwand Alsino <parwand.alsino@hhu.de>
2022-08-10 21:10:57 +0200

    linked app with flask_sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['ALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 
 
 @app.route('/')
