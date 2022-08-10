15c24ae542324666fa761bcee1e16ba56353af1a
Parwand Alsino <parwand.alsino@hhu.de>
2022-08-10 22:03:56 +0200

    add post function implemented

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
-db.create_all()
 
 
 # create a task model for database
@@ -14,10 +13,28 @@ class Task(db.Model):
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("index.html")
+def index():  # put application's code here
+    task = Task.query.order_by(Task.date_created).all()
+    return render_template("index.html", task=task)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        task = request.form['task']
+        new_task = Task(content=task)
+        try:
+            db.create_all()
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issues where adding your input"
 
 
 if __name__ == '__main__':
