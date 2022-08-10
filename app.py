52e405f328743e179ec6aba062447c208ae94ca6
Parwand Alsino <parwand.alsino@hhu.de>
2022-08-10 21:19:46 +0200

    created a model

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['ALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
+
+
+# create a task model for database
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+
 
 @app.route('/')
 def hello_world():  # put application's code here
